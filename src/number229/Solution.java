547c0c4582fb5acefdaaa5ab01606cb0a67557be
Qin-Xiang <1584381748@qq.com>
2019-06-08 13:35:05 +0800

    229

--- src/number229/Solution.java
+++ src/number229/Solution.java
@@ -1,12 +1,15 @@
-package number169;
+package number229;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Solution {
-    public int majorityElement(int[] nums) {
-    	int max = 0;
-    	int res = 0;
+	
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+    	int max = (int) Math.floor(nums.length / 3);
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
         	if(map.containsKey(nums[i]))
@@ -16,17 +19,17 @@ public class Solution {
         }
         
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-        	if(entry.getValue() > max){
-        		max = entry.getValue();
-        		res = entry.getKey();
-        	}
+        	if(entry.getValue() > max)
+        		res.add(entry.getKey());
         }
         return res;
     }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Solution s = new Solution();
+		int[] nums = {1, 2};
+		System.out.println(s.majorityElement(nums));
 	}
 
 }
