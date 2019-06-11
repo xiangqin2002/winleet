c53ba27943e07561ef18a88a97c399de54b58184
Qin-Xiang <1584381748@qq.com>
2019-06-11 17:54:07 +0800

    92

--- src/number92/Solution.java
+++ src/number92/Solution.java
@@ -1,7 +1,52 @@
 package number92;
 
-public class Solution {
 
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+  
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode dhead = null;
+        ListNode phead = null;
+        ListNode next = cur.next; 
+        int c = 1;
+        
+        while(c <= n){
+        	if(c == m){
+        		phead = pre;
+        		dhead = cur;
+        	}
+        	
+        	if(c >= m + 1){
+        		cur.next = pre;
+        		pre = cur;
+        		cur = next;
+        		if(next != null)
+        			next = next.next;
+        	}
+        	else{
+	        	pre = cur;
+	        	cur = next;
+	        	if(cur != null)
+	        		next = next.next;
+        	}
+        	c++;
+        	if(c == n + 1){
+        		dhead.next = cur;
+        		if(phead != null)
+        			phead.next = pre;
+        		else
+        			head = pre;
+        	}
+        }
+        return head;
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
