b5142dde80dacdedea8603b0ad88b218d608cd68
Qin-Xiang <18671803827@163.com>
2019-08-19 22:55:54 +0800

    94

--- src/number94/Solution.java
+++ src/number94/Solution.java
@@ -1,7 +1,30 @@
 package number94;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
 
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+public class Solution {
+	List<Integer> res = new ArrayList<>();
+	
+    public List<Integer> inorderTraversal(TreeNode root) {
+    	inOrder(root);
+    	return res;
+    }
+    
+    private void inOrder(TreeNode node){
+    	if(node != null){
+    		inOrder(node.left);
+    		res.add(node.val);
+    		inOrder(node.right);
+    	}
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
